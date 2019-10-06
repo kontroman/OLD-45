542b5ff7a849d1637ee5f91b815444e45b4b2f7e
Kontroman <ckontro@gmail.com>
2019-10-06 14:43:30 +0600

    BackUp

--- OLD-45(project)/Assets/Scripts/Enemy.cs
+++ OLD-45(project)/Assets/Scripts/Enemy.cs
@@ -7,6 +7,6 @@ public class Enemy : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
-            GameController.RestartGame();
+            GameController.RestartScene();
     }
 }
---
+++ OLD-45(project)/Assets/Scripts/SafeTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SafeTrigger : MonoBehaviour
+{
+    public GameObject field;
+
+    private void Start()
+    {
+        field.SetActive(false);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            field.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            field.SetActive(false);
+        }
+    }
+}
